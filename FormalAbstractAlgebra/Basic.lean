865dd6d72a7c634497e1f55b71a13ffb42e2568c
Advay Phadke <amphadke@proton.me>
2026-01-29 15:42:21 +0000

    addition theorems in Nat

---
+++ FormalAbstractAlgebra/Basic.lean
@@ -0,0 +1,59 @@
+-- neutrality of 0 --
+
+theorem zeroadd (n : Nat) : 0 + n = n := by
+    induction n with
+    | zero => rfl
+    | succ d hd =>
+        rw[Nat.add_succ]
+        rw[hd]
+        rfl
+
+-addition and successor--
+
+theorem s_add (a b : Nat) : Nat.succ a + b = Nat.succ (a + b) := by
+    induction b with
+    | zero =>
+        rw[Nat.add_zero]
+        rw[Nat.add_zero]
+        rfl
+    | succ d hd =>
+        rw[Nat.add_succ]
+        rw[Nat.add_succ]
+        rw[hd]
+        rfl
+
+-commutativity--
+
+theorem comm (a b : Nat) : a + b = b + a := by
+    induction a with
+        | zero =>
+            induction b with
+                | zero =>
+                    rw[zeroadd]
+                    rfl
+                | succ d hd =>
+                    rw[Nat.add_zero]
+                    rw[zeroadd]
+        | succ c hc =>
+            rw[Nat.add_succ]
+            rw[Nat.add_zero]
+            rw[s_add]
+            rw[Nat.add_succ]
+            rw[hc]
+            rfl
+
+--associativity--
+
+theorem assoc (a b c : Nat) : (a + b) + c = a + (b + c) := by
+    induction c with
+        | zero =>
+            rw[Nat.add_zero]
+            rw[Nat.add_zero]
+            rfl
+        | succ d hd =>
+            rw[Nat.add_succ]
+            rw[Nat.add_succ]
+            rw[Nat.add_succ]
+            rw[hd]
+            rfl
+
